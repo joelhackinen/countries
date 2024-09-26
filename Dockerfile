24aeef6fe7d835efdad0db517edb9ffafeb0ec66
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-09-26 18:34:58 +0300

    migrate to vite

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
-FROM node:16.18.1-bullseye-slim AS build-stage
+FROM node:20.17.0-bullseye-slim AS build-stage
 
 WORKDIR /usr/src/app
 
 COPY . .
 
-RUN npm ci --silent
+RUN npm ci
 
 RUN npm run build
 
 
-FROM nginx:1.24.0-alpine
+FROM nginx:1.27.0-alpine
 
 COPY --from=build-stage /usr/src/app/build /usr/share/nginx/html
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import store from "./store";
 import router from "./router";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>,
-);
+)
